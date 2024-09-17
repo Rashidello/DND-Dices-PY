81dc7db118ff7dfb1f22d2a6a57a4fc192817cd1
Rashidellø <86264484+Rashidello@users.noreply.github.com>
2024-09-17 13:26:25 +0400

    Update Dice.py

--- Dice.py
+++ Dice.py
@@ -1,6 +1,6 @@
 import random
 
-i =  int(input("choose a dice by a number\n(d4,d5,d6,d8,d10,d20,d100)\n"))
+i =  int(input("choose a dice by a number\n(d4,d5,d6,d8,d10,d20\n"))
 
 Dice = random.randint(1,i)
 
